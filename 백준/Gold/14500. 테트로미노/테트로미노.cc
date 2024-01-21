9cda4873ccd892fd373a364dc4306bdbdfd432b1
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-01-21 16:52:54 +0900

    [Gold IV] Title: 테트로미노, Time: 80 ms, Memory: 3980 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -1,57 +1,77 @@
-#include<iostream>
-#include<algorithm>
-#include<cstring>
-using namespace std;
-void getMaxNum(int x, int y, int cnt, int curScore);
-int map[501][501], visit[501][501];
-int dx[] = {-1, 0, 0, 1};
-int dy[] = {0, -1, 1, 0};
-int N, M, ans=0;
-int main(){
-    memset(visit, 0, sizeof(visit));
-    cin >> N >> M;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=M; j++){
-            cin >> map[i][j];
-        }
-    }
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <tuple>
+#include <map>
+#include <string>
+#include <vector>
 
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=M; j++){
-            visit[i][j] = 1;
-            getMaxNum(i, j, 1, map[i][j]);
-            visit[i][j] = 0; // 백트래킹
-        }
-    }
-    cout << ans;
+using namespace std;
+int maap[501][501], visit[501][501];
+int dx[] = { -1, 0, 0, 1 };
+int dy[] = { 0, -1, 1, 0 };
+int N, M, ans = 0;
+
+void dfs(int i, int j, int cnt, int current) {
+	if (cnt == 4) {
+		if (ans < current) {
+			ans = current;
+		}
+		return;
+	}
+
+	for (int k = 0; k < 4; k++) {
+		int nx = i + dx[k];
+		int ny = j + dy[k];
+		if (nx >= 0 && nx < N && ny>=0 && ny < M && visit[nx][ny] == 0) {
+			visit[nx][ny] = 1;
+			dfs(nx, ny, cnt + 1, current + maap[nx][ny]);
+			visit[nx][ny] = 0;
+		}
+	}
+
+	if (i - 1 >= 0 && j - 1 >= 0 && i + 1 < N) { //ㅓ
+		ans = max(ans, (maap[i - 1][j] + maap[i][j - 1] + maap[i][j] + maap[i + 1][j]));
+	}
+	if (i - 1 >= 0 && j + 1 < M && i + 1 < N) { //ㅏ
+		ans = max(ans, (maap[i - 1][j] + maap[i][j + 1] + maap[i][j] + maap[i + 1][j]));
+	}
+	if (j - 1 >= 0 && j + 1 < M && i + 1 < N) { //ㅗ
+		ans = max(ans, (maap[i][j] + maap[i + 1][j] + maap[i + 1][j - 1] + maap[i + 1][j + 1]));
+	}
+	if (j - 1 >= 0 && j + 1 < M && i + 1 < N) { //ㅜ
+		ans = max(ans, (maap[i][j - 1] + maap[i][j] + maap[i][j + 1] + maap[i + 1][j]));
+	}
 }
 
-void getMaxNum(int x, int y, int cnt, int curScore){
-    if(cnt == 4) { // 깊이가 4가 되면
-        if(ans < curScore) ans = curScore;
-        return;
-    }
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx < 1 || ny < 1 || nx > N || ny > M || visit[nx][ny]) continue;
-        visit[nx][ny] = 1;
-        getMaxNum(nx, ny, cnt+1, curScore+map[nx][ny]);
-        visit[nx][ny] = 0; // 백트래킹
-    }
+	for (int i = 0; i < 501; i++) {
+		for (int j = 0; j < 501; j++) {
+			visit[i][j] = 0;
+		}
+	}
 
-    if(x-1 >= 1 && y-1 >= 1 && x+1 <= N) { //ㅓ
-        ans = max(ans, (map[x-1][y] + map[x][y-1] + map[x][y] + map[x+1][y]));
-    }
-    if(x-1 >= 1 && y+1 <= M && x+1 <= N) { //ㅏ
-        ans = max(ans, (map[x-1][y] + map[x][y+1] + map[x][y] + map[x+1][y]));
-    }
-    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { //ㅗ
-        ans = max(ans, (map[x][y] + map[x+1][y] + map[x+1][y-1] + map[x+1][y+1]));
-    }
-    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { //ㅜ
-        ans = max(ans, (map[x][y-1] + map[x][y] + map[x][y+1] + map[x+1][y]));
-    }
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int temp = 0;
+			cin >> temp;
+			maap[i][j] = temp;
+		}
+	}
 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			visit[i][j] = 1;
+			dfs(i, j, 1, maap[i][j]);
+			visit[i][j] = 0;
+		}
+	}
+	cout << ans;
 }
