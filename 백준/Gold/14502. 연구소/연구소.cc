20b8b5f799ca2b0614c5bc83a5af66cc105ab326
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-01-23 17:48:35 +0900

    [Gold IV] Title: 연구소, Time: 248 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,106 +1,111 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <algorithm>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <tuple>
+#include <map>
+#include <unordered_set> 
 using namespace std;
 
-int n, m = 0;
-int map[8][8];
-int tempmap[8][8];
-int result=0;
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,1,-1 };
-
-void copy(int tmp[8][8], int a[8][8]) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			tmp[i][j] = a[i][j];
+int N, M, result = 0;
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,-1,1};
+int mymap[8][8] = { 0, };
+int tempmap[8][8] = { 0, };
+void copy() {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			tempmap[i][j] = mymap[i][j];
 		}
 	}
 }
 
-void SpreadVirus() {
-	//1. 일단 2인거 찾고 push하기 2. 2상하좌우 확인하고 0이면 2 바꾸고 push (while문으로 q 0일때까지) 3. 이거 다하면 count하고 max값이랑 비교해서 크면 집어넣기
-	int spreadmap[8][8];
-	queue < pair<int, int>> q;
-	copy(spreadmap, tempmap);
-	//1번
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (spreadmap[i][j] == 2) {
-				q.push({ i, j });
-			}
+void dfs(int i, int j) {
+	for (int x = 0; x < 4; x++) {
+		int nx = i + dx[x];
+		int ny = j + dy[x];
+		if (nx >= 0 && ny >= 0 && nx < N && ny < M && tempmap[nx][ny] == 0) {
+			tempmap[nx][ny] = 2;
+			dfs(nx, ny);
 		}
 	}
-	//2번
-	while (q.size()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-			if (nx<0 || nx>=n || ny<0 || ny>=m) { continue; }
-			if (spreadmap[nx][ny] == 0) {
-				spreadmap[nx][ny] = 2;
-				q.push({ nx, ny });
-			}
-		}
-	}
-
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (spreadmap[i][j] == 0) {
-				cnt++;
-			}
-		}
-	}
-
-	result = max(result, cnt);
 }
 
-void wall(int cnt) {
-	if (cnt == 3) {
-		SpreadVirus();
+int check() {
+	int safe = 0;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (tempmap[i][j] == 0) {
+				safe++;
+			}
+		}
+	}
+	return safe;
+}
+
+void spreadVirus() {
+	copy();
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (tempmap[i][j] == 2) {
+				dfs(i, j);
+			}
+		}
+	}
+
+	int safe = check();
+	if (safe > result) {
+		result = safe;
+	}
+
+}
+
+void initiate() {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			tempmap[i][j] = 0;
+		}
+	}
+}
+
+void make_wall(int cnt) {
+	if (cnt == 2) {
+		spreadVirus();
+		initiate();
 		return;
 	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (tempmap[i][j] == 0) {
-				tempmap[i][j] = 1;
-				wall(cnt+1);
-				tempmap[i][j] = 0;//SpreadVirsu에서 bfs랑 count까지 전부다하고 나머지 0으로 바꿔줘야함
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (mymap[i][j] == 0) {
+				cnt++;
+				mymap[i][j] = 1;
+				make_wall(cnt);
+				cnt--;
+				mymap[i][j] = 0;
 			}
 		}
 	}
-
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			int t = 0;
-			cin >> t;
-			map[i][j] = t;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int temp = 0;
+			cin >> temp;
+			mymap[i][j] = temp;
 		}
 	}
 
-	//3개 무작위 골라서 벽세우기 함수 만약 하나 벽세우면 wall함수를 이용해서 나머지 2개 벽 다시 세워주기
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] == 0) {
-				copy(tempmap,map);
-				tempmap[i][j] = 1;
-				wall(1);
-				tempmap[i][j] = 0; //맨처음 1넣어준거 0으로 바꿔준느거임 wall함수에서 나머지 2개 1박아둔거 안에서 0으로 다시 되돌려줘야함
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			int cnt = 0;
+			if (mymap[i][j] == 0) {
+				mymap[i][j] = 1;
+				make_wall(cnt);
+				mymap[i][j] = 0;
 			}
 		}
 	}
