b5e35109ca69b09a68cd39d286de06c1d42bfdd5
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-03-29 13:32:34 +0900

    [level 3] Title: 불량 사용자, Time: 4.11 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,55 +1,50 @@
 #include <string>
 #include <vector>
-#include <set>
 #include <algorithm>
+#include <iostream>
+#include <map>
+
 using namespace std;
 
-bool correct(string bid, string uid){
-    for(int i=0;i<bid.size();i++){
-        if(bid[i]=='*'){
-            continue;
-        }else if(bid[i]!=uid[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
 int solution(vector<string> user_id, vector<string> banned_id) {
-    vector <int> v;
+    int answer = 0;
+    vector<int>v;
     for(int i=0;i<user_id.size();i++){
         v.push_back(i);
     }
-    set<string> s;
+    vector<string> t;
+    int y=0;
+    map<int, vector<int>> map;
     do{
-        vector<string> target;
-        vector<string> myfind;
-        for(int i=0;i<banned_id.size();i++){
-            target.push_back(banned_id[i]);
-        }
-        
+        vector<int>yy;
+        int k=0;
+        bool check[9]={false,};
         for(int i=0;i<v.size();i++){
-            string uid=user_id[v[i]];
-            for(int j=0;j<target.size();j++){
-                string temp = target[j];
-                if(temp.size()==uid.size() && correct(temp,uid)){
-                    target.erase(target.begin()+j);
-                    myfind.push_back(uid);
-                    break;
+            for(int j=0;j<banned_id.size();j++){
+                if(!check[j] && user_id[v[i]].size()==banned_id[j].size()){
+                    int x=0;
+                    for(int a=0;a<banned_id[j].size();a++){
+                        if(banned_id[j][a]=='*'){
+                            x++;
+                        }else if(banned_id[j][a]==user_id[v[i]][a]){
+                            x++;
+                        }
+                    }
+                    if(x==user_id[v[i]].size()){
+                        check[j]=true;
+                        k++;
+                        yy.push_back(v[i]);
+                        yy.push_back(j);
+                        break;
+                    }
                 }
             }
         }
-        
-        if(myfind.size()==banned_id.size()){
-        sort(myfind.begin(),myfind.end());
-        string tmp="";
-        for(int i=0;i<myfind.size();i++){
-            tmp+=myfind[i];
+        if(k==banned_id.size()){
+            map[y]=yy;
+            y++;
         }
-        s.insert(tmp);
-        }
-        
     }while(next_permutation(v.begin(),v.end()));
     
-    return s.size();
+    return map.size();
 }
