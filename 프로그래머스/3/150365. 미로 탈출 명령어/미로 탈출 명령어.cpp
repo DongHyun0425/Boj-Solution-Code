875ecc2e9c86a40c175425b398897b049f93bd6e
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-05-02 15:56:14 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.cpp
@@ -1,70 +1,57 @@
 #include <string>
+#include <iostream>
 #include <vector>
-
 using namespace std;
 
-struct Delta {
-    string command = "dlru";
-    vector<int> dx {1, 0, 0, -1};
-    vector<int> dy {0, -1, 1, 0};
-};
+int targetr=0;
+string answer="";
+int targetc=0;
+int flag =0;
+int N=0;
+int M=0;
+int dx[4]={1,0,0,-1};
+int dy[4]={0,-1,1,0};
+char addstring[4]={'d','l','r','u'};
 
-bool OOB (int x, int y, int n, int m){
-    if ( 0 < x && x <= n && 0 < y && y <= m){
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-void dfs(int x, int y, vector<int>& info, const Delta& delta, string& answer, string& stack, int depth, bool& dfsExit){
-    if (dfsExit){
+void dfs(int x, int y ,int cnt,int k,string s){
+    if(flag){
         return;
     }
-    int n = info[0];
-    int m = info[1];
-    int r = info[2];
-    int c = info[3];
-    int k = info[4];
-
-    if (k - depth < abs(x-r) + abs(y-c)){
+    if(k-cnt<abs(x-targetr)+abs(y-targetc)){
         return;
     }
-
-    // printf("x: %d, y: %d, depth: %d\n", x, y, depth);
-    if (depth == k){
-        if (x == r && y == c){
-            // cout << stack << endl;
-            answer = stack;
-            dfsExit = true;
+    
+    if(cnt==k){
+        if(x==targetr && y == targetc ){
+        flag++;
+        answer+=s;
         }
         return;
     }
     
-    for (int d=0; d < 4; d++){
-        int nextX = x + delta.dx[d];
-        int nextY = y + delta.dy[d];
-        if (OOB(nextX, nextY, n, m)){
-            stack += delta.command[d];
-            dfs(nextX, nextY, info, delta, answer, stack, depth+1, dfsExit);
-            stack = stack.erase(stack.size()-1);
-        }
+    
+    for(int i=0;i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx>=1 && nx<=N && ny>=1 && ny <=M){
+                s+=addstring[i];
+                dfs(nx,ny,cnt+1,k,s);
+                s.pop_back();
+            }
     }
 }
 
 string solution(int n, int m, int x, int y, int r, int c, int k) {
-    string answer = "";
-    string stack = "";
-    vector<int> info {n, m, r, c, k};
-    Delta delta;
-    bool dfsExit = false;
-    int remain = abs(x-r) + abs(y-c);
-    if ((k - remain) % 2 != 0 || remain > k) {
-        answer = "impossible";
-    }
-    else {
-        dfs(x, y, info, delta, answer, stack, 0, dfsExit);
+    N=n;
+    M=m;
+    targetr = r;
+    targetc = c;
+    int remain = abs(x-r)+abs(y-c);
+    if((k-remain) % 2!=0 || remain > k){
+       answer = "impossible"; 
+    }else{
+        string s="";
+        dfs(x,y,0,k,s);
     }
     return answer;
 }
