27b3c18c2b746ca7fae0e02c220fe0c670bc4727
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-07-16 16:51:33 +0900

    [level 2] Title: 튜플, Time: 13.21 ms, Memory: 5.57 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065#qna) 
 
 ### 성능 요약
 
-메모리: 6.02 MB, 시간: 1.48 ms
+메모리: 5.57 MB, 시간: 13.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 17:25:02
+2024년 07월 16일 16:51:28
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.cpp
+++ 프로그래머스/2/64065. 튜플/튜플.cpp
@@ -1,57 +1,41 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <iostream>
+#include <map>
+#include <algorithm>
 using namespace std;
 
+map<int,int>m;
+
+bool comp(pair<int,int>&a,pair<int,int>&b){
+    return a.second>b.second;
+}
+
 vector<int> solution(string s) {
-    vector <int> map[100000];
     vector<int> answer;
-    string temp;
-    bool sw = false;
-    vector <int> v;
-    int cnt = 1;
-    for (int i = 1; i < s.size() - 1; i++) {
-        if (s[i] == '{') { sw = true; } 
-        else if (s[i] == '}') {
-            sw = false;
-            v.push_back(stoi(temp));
-            temp = "";
-        }
-        else if (sw && s[i] != ',') {
-            temp += s[i];
-        }else if (sw && s[i] == ',') {
-            v.push_back(stoi(temp));
-            temp = "";
-        }
-       else if (!sw && s[i] == ',') {
-            for (int i = 0; i < v.size(); i++) {
-                map[v.size()].push_back(v[i]);
-            }
-            cnt++;
-            v.clear();
-        }
-    }
-
-    for (int i = 0; i < v.size(); i++) {
-        map[v.size()].push_back(v[i]);
-    }
-
-    bool check[100000] = { false, };
-    for (int i = 1; i < cnt+1; i++) {
-        if (i == 1) {
-            answer.push_back(map[i][0]);
-            check[map[i][0]] = true;
-        }
-        else {
-            for (int j = 0; j < map[i].size(); j++) {
-                if (!check[map[i][j]]) {
-                    answer.push_back(map[i][j]);
-                    check[map[i][j]] = true;
-                }
-            }
-        }
-    }
-    return answer;
     
+    
+    string tmp="";
+    for(int i=0;i<s.size();i++){
+        if(s[i]!='{'){
+            if(s[i]=='}'||s[i]==','){
+                if(tmp.size()>0){
+                    m[stoi(tmp)]++;
+                    tmp="";
+                }
+            }else{
+                tmp+=s[i];
+            }
+        }
+    }
+    
+    vector<pair<int,int>>v(m.begin(),m.end());
+    
+    sort(v.begin(),v.end(),comp);
+    
+    for(int i=0;i<v.size();i++){
+        answer.push_back(v[i].first);
+    }
+    
+    return answer;
 }
