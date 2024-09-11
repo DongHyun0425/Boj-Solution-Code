1bdfa6bad61708d1941f2897cdafd8f4711f79da
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-09-11 21:27:38 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 22.64 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,95 +1,60 @@
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
 #include <string>
-#include <cmath>
-#include <tuple>
+#include <vector>
+#include <iostream>
 #include <map>
-
+#include <sstream>
 using namespace std;
 
-map <string, int> mymap;
-int givetake[50][50] = { 0, };
-int jisu[50][3] = { 0, };
+int map1[51][51];
+int map2[51][3];
+map<string,int> table;
 int solution(vector<string> friends, vector<string> gifts) {
-    	int answer = 0;
-	int cnt = 0;
-	//1
-	for (int i = 0; i < friends.size(); i++) {
-		mymap.insert(make_pair(friends[i], cnt));
-		cnt++;
-	}
-	//2
-	for (int i = 0; i < gifts.size(); i++) {
-		string a = "";
-		string b = "";
-		int char_count = 0;
-		for (int j = 0; j < gifts[i].size(); j++) {
-			if (gifts[i][j] == ' ') { char_count++; continue; }
-			if (char_count == 0) {
-				a += gifts[i][j];
-			}
-			else {
-				b += gifts[i][j];
-			}
-		}
-
-		int a_, b_ = 0;
-		for (int i = 0; i < mymap.size(); i++) {
-			auto it = mymap.find(a);
-			a_ = it -> second;
-		}
-		for (int i = 0; i < mymap.size(); i++) {
-			auto it = mymap.find(b);
-			b_ = it->second;
-		}
-		givetake[a_][b_] += 1;
-	}
-	//3
-	for (int i = 0; i < friends.size(); i++) {
-		int a_ = 0;
-		for (int j = 0; j < mymap.size(); j++) {
-			auto it = mymap.find(friends[i]);
-			a_ = it->second;
-		}
-		int give = 0;  int get = 0;
-		for (int x = 0; x < friends.size(); x++) {
-			give += givetake[a_][x];
-			get += givetake[x][a_];
-		}
-		jisu[i][0] = give;
-		jisu[i][1] = get;
-		jisu[i][2] = give-get;
-	}
-	//4
-	int result[50] = { 0, };
-	for (int i = 0; i < friends.size(); i++) {
-		for (int j = 0; j < friends.size(); j++) {
-			int give = givetake[i][j];
-			int get = givetake[j][i];
-			if (give > get) {
-				result[i] += 1;
-			}
-			else if (give < get) {
-				result[j] += 1;
-			}
-			else if ((give == 0 && get == 0) || (give == get)) {
-				if (jisu[i][2] > jisu[j][2]) {
-					result[i] += 1;
-				}
-				else if (jisu[i][2] < jisu[j][2]) {
-					result[j] += 1;
-				}
-			}
-		}
-	}
-
-	for (int i = 0; i < 50; i++) {
-		if (result[i] > answer) {
-			answer = result[i];
-		}
-	}
-	return answer/2;
+    int answer = 0;
+    for(int i=0;i<friends.size();i++){
+        table.insert({friends[i],i});
+    }
+    for(int i=0;i<gifts.size();i++){
+        string give="";
+        string get = "";
+        vector<string>v;
+        stringstream ss(gifts[i]);
+        string tmp;
+        while(ss>>tmp){
+            v.push_back(tmp);
+        }
+        
+        give=v[0];
+        get=v[1];
+        
+        int give_idx=0;
+        int get_idx=0;
+        for(auto iter: table){
+            if(iter.first==give) give_idx = iter.second;
+            if(iter.first==get) get_idx = iter.second;
+        }
+        
+        map1[give_idx][get_idx]++;
+        map2[give_idx][0]++;
+        map2[get_idx][1]++;
+    }
+    for(int i=0;i<friends.size();i++){
+        map2[i][2] = map2[i][0]-map2[i][1];
+    }
+    
+    for(int i=0;i<friends.size();i++){
+        int tmp=0;
+        for(int j=0;j<friends.size();j++){
+            if(i==j) continue;
+            if(map1[i][j]!=0 && map1[i][j]>map1[j][i]){
+                tmp++;
+            }else{
+                //내가 안줬는데 상대는 나한테 준경우 어차피 상대 측에서 ++할거임
+                if((map1[i][j]==map1[j][i]) && map2[i][2]>map2[j][2]){
+                    tmp++;
+                }
+            }
+        }
+        if(answer<tmp) answer=tmp;
+    }
+    return answer;
 }
