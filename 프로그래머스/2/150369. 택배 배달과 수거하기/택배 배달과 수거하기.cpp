9e914427c309c82e3f0fa0399e45d7389f2d1aa2
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-10-08 14:18:31 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 2.58 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -1,47 +1,23 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
-    long long answer = -1;
-    for (int i = n-1; i >= 0; --i) {
-        int flag = 0;
-        if (deliveries[i] > 0 || pickups[i] > 0) {
-            flag = i;
-            //give box
-            int checkbox = 0;
-            for (int j = i; j >= 0; j--) {
-                if ((checkbox + deliveries[j]) <= cap) {
-                    checkbox += deliveries[j];
-                    deliveries[j] = 0;
-                }
-                else {
-                    int tmp = cap - checkbox;
-                    checkbox = cap;
-                    deliveries[j] -= tmp;
-                    break;
-                }
-            }
-
-            //get box
-            int getbox = 0;
-            for (int j = i; j >= 0; j--) {
-                if ((getbox + pickups[j]) <= cap) {
-                    getbox += pickups[j];
-                    pickups[j] = 0;
-                }
-                else {
-                    int tmp = cap - getbox;
-                    getbox = cap;
-                    pickups[j] -= tmp;
-                    break;
-                }
-            }
-            answer += (i + 1) * 2;
-            if (deliveries[flag] > 0 || pickups[flag] > 0) i = flag+1;
+    long long answer = 0;
+    long long d=0;
+    long long p=0;
+    for(int i = n-1; i>=0; i--){
+        d-=deliveries[i];
+        p-=pickups[i];
+        int cnt =0;
+        while(d<0 || p <0){
+            d+=cap;
+            p+=cap;
+            cnt++;
         }
+        answer += (i+1)*2*cnt;
     }
-    return answer + 1;
+    
+    return answer;
 }
