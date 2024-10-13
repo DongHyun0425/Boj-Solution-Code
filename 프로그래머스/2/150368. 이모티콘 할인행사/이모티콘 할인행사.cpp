f7e1857c3838746ae069e5a25e44fa90173c047c
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2024-10-13 17:39:42 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 22.12 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.cpp
@@ -1,69 +1,60 @@
 #include <string>
 #include <vector>
 #include <iostream>
+
 using namespace std;
-int cnt = 0;
-vector<int> d;
-vector<int> v;
-vector<vector<int>> u;
-vector<int> e;
-
-int maxuser=0;
-int maxprice=0;
-
-//현재 v에 이모티콘은 [1300, 1500, 1600, 4900] [10,10,20,30]들어있음
-void check(){
-    int adduser=0;
-    int addprice=0;
-    for(int i=0;i<u.size();i++){
-        int uprate = u[i][0];
-        int upprice = u[i][1];
-        int price = 0;
+int e_size=0;
+vector<int> answer;
+int d[4]={10,20,30,40};
+vector<vector<int>> users;
+vector<int> emoticons;
+void cal(vector <int> &v){
+    int plus = 0;
+    int T_A =0;
+    for(int i=0;i<users.size();i++){
+        int rate = users[i][0];
+        int amount = users[i][1];
+        int tmp = 0;
         for(int j=0;j<v.size();j++){
-            if(uprate<=v[j]){
-                price += (e[j] * (100-v[j]))/100;
+            if(rate <=v[j]){
+                tmp +=(emoticons[j]*(100-v[j]))/100;
             }
         }
-        if(price>=upprice){
-            adduser++;
+        if(amount <= tmp){
+            plus++;
         }else{
-            addprice += price;
+            T_A += tmp;
         }
     }
-    if(adduser>maxuser){
-        maxuser = adduser;
-        maxprice = addprice;
-    }else if(adduser==maxuser && addprice>maxprice){
-        maxuser = adduser;
-        maxprice = addprice;
+    if(plus>answer[0]){
+        answer[0]=plus;
+        answer[1]=T_A;
+    }else if(plus == answer[0] && T_A > answer[1]){
+        answer[1]=T_A;//?+=
     }
 }
 
-void A(int c){
+void discount_combi(vector<int>&v){
+    if(v.size()==e_size){
+        cal(v);
+        return;
+    }
+    
     for(int i=0;i<4;i++){
         v.push_back(d[i]);
-        c++;
-        if(c<cnt){
-            A(c);
-        }else{
-            check();
-        }
-        c--;
+        discount_combi(v);
         v.pop_back();
     }
 }
 
-vector<int> solution(vector<vector<int>> users, vector<int> emoticons) {
-    u=users;
-    e=emoticons;
-    vector<int> answer;
-    cnt=emoticons.size();
-    d.push_back(10);
-    d.push_back(20);
-    d.push_back(30);
-    d.push_back(40);
-    A(0);
-    answer.push_back(maxuser);
-    answer.push_back(maxprice);
+
+vector<int> solution(vector<vector<int>> userss, vector<int> emoticonss) {
+    emoticons = emoticonss;
+    users=userss;
+    answer.push_back(0);
+    answer.push_back(0);
+    e_size=emoticons.size();
+    vector<int>v;
+    discount_combi(v);
     return answer;
 }
