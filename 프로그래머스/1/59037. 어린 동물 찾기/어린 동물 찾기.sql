7096bd024ff772028d08f3f7ea76ee9650f2b8e3
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2025-07-24 14:48:09 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,2 +1,2 @@
 -- 코드를 입력하세요
-SELECT animal_id,name from animal_ins where INTAKE_CONDITION != "aged" order by animal_id
+SELECT ANIMAL_ID,NAME from ANIMAL_INS where INTAKE_CONDITION != "Aged" order by ANIMAL_ID
