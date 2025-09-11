c841e6e721ba1d137a98bcc0f7deaaa0deca3f47
DongHyun0425 <137744231+DongHyun0425@users.noreply.github.com>
2025-09-11 11:49:45 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+select ID,EMAIL,FIRST_NAME,LAST_NAME
+from DEVELOPERS
+where SKILL_CODE & (
+select CODE
+from SKILLCODES 
+where NAME like "Python") > 0
+or SKILL_CODE & (
+select CODE
+from SKILLCODES 
+where NAME like "C#") > 0
+order by ID
